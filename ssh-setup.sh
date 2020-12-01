7af6edc824f1895df82cab2204f4cdabcdaf6861
Ivander <ivanderjmw@gmail.com>
2020-12-02 02:07:48 +0800

    Add ssh keygen

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -1,6 +1,9 @@
 PI_NUMBER=`cat config.txt`
 SERVER_IP=`cat server.txt`
 
+ssh-keygen
+ssh-copy-id orca@${SERVER_IP}
+
 content="#!/bin/bash
 myIP=`hostname -I | tr -d \" \"`
 echo \$myIP | ssh orca@${SERVER_IP} \"cat - > ip${PI_NUMBER}.txt\""
