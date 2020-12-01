58d42174826f31758ee1ddd3bd9c1330dd6ed2c1
Ivander <ivanderjmw@gmail.com>
2020-12-02 02:00:45 +0800

    Fix wrong directory

--- ssh-setup.sh
+++ ssh-setup.sh
@@ -8,6 +8,6 @@ echo $myIP | ssh orca@${SERVER_IP} \"cat - > ip${PI_NUMBER}.txt\""
 touch /home/pi/setIP.sh
 echo "$content" > /home/pi/setIP.sh
 
-chmod a+x setIP.sh
+chmod a+x /home/pi/setIP.sh
  
 sudo -c 'echo "sudo -i -u pi /home/pi/setIP.sh" >> /etc/xdg/lxsession/LXDE-pi/autostart'
